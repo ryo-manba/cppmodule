c2c287fe362e47e77daaf0901c42ddd08c569d65
Ryomanba <datemanba3@gmail.com>
2021-11-20 13:44:22 +0900

    comment fix

--- cpp00/ex02/tests.cpp
+++ cpp00/ex02/tests.cpp
@@ -12,16 +12,16 @@
 #include <functional>
 #include "Account.hpp"
 
-
+#include <iostream>
 int		main( void ) {
 
 	typedef std::vector<Account::t>							  accounts_t; // Account型の配列をaccounts_tにしてる
-	typedef std::vector<int>								  ints_t; // intの配列を ints_tにしてる
+	typedef std::vector<int>								  ints_t;     // intの配列を ints_tにしてる
 	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
 
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	accounts_t				accounts( amounts, amounts + amounts_size ); // 2つAccount作る
+	accounts_t				accounts( amounts, amounts + amounts_size );  // beginとendを指定
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
 
