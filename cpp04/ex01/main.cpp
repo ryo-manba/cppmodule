35eaa22578261bb5fede069afdc910150cf02334
Ryomanba <datemanba3@gmail.com>
2022-01-10 18:56:24 +0900

    fix: ex01 main

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -34,40 +34,40 @@ static void animalInit(Animal *animal[SIZE])
 
 static void brainTestCat()
 {
-    Cat cat, cat2;
+    Cat cat1, cat2;
     std::string str = "hello";
-    cat.setIdeas(INDEX, str);
-    cat2 = cat;
+    cat1.setIdeas(INDEX, str);
+    cat2 = cat1;
     colorStart(YELLOW);
-    std::cout << "cat ideas = " << cat.getIdeas(INDEX) << std::endl;
-    std::cout << "cat2 ideas = " << cat2.getIdeas(INDEX) << std::endl;
+    std::cout << "cat1 ideas[" << INDEX << "] = " << cat1.getIdeas(INDEX) << std::endl;
+    std::cout << "cat2 ideas[" << INDEX << "] = " << cat2.getIdeas(INDEX) << std::endl;
     colorEnd();
     str = "world";
     cat2.setIdeas(INDEX, str);
     colorStart(CYAN);
     std::cout << "-----DeepCopy-----" << std::endl;
     std::cout << "ideas[" << INDEX << "] :" << std::endl;
-    std::cout << "cat ideas = " << cat.getIdeas(INDEX) << std::endl;
+    std::cout << "cat1 ideas = " << cat1.getIdeas(INDEX) << std::endl;
     std::cout << "cat2 ideas = " << cat2.getIdeas(INDEX) << std::endl;
     colorEnd();
 }
 
 static void brainTestDog()
 {
-    Dog dog, dog2;
+    Dog dog1, dog2;
     std::string str = "hello";
-    dog.setIdeas(INDEX, str);
-    dog2 = dog;
+    dog1.setIdeas(INDEX, str);
+    dog2 = dog1;
     colorStart(YELLOW);
-    std::cout << "dog ideas = " << dog.getIdeas(INDEX) << std::endl;
-    std::cout << "dog2 ideas = " << dog2.getIdeas(INDEX) << std::endl;
+    std::cout << "dog1 ideas[" << INDEX << "] = " << dog1.getIdeas(INDEX) << std::endl;
+    std::cout << "dog2 ideas[" << INDEX << "] = " << dog2.getIdeas(INDEX) << std::endl;
     colorEnd();
     str = "world";
     dog2.setIdeas(INDEX, str);
     colorStart(CYAN);
     std::cout << "-----DeepCopy-----" << std::endl;
     std::cout << "ideas[" << INDEX << "] :" << std::endl;
-    std::cout << "dog ideas = " << dog.getIdeas(INDEX) << std::endl;
+    std::cout << "dog1 ideas = " << dog1.getIdeas(INDEX) << std::endl;
     std::cout << "dog2 ideas = " << dog2.getIdeas(INDEX) << std::endl;
     colorEnd();
 }
@@ -83,6 +83,5 @@ int main()
     brainTestCat();
     std::cout << "\n-----Dog Brain test-----\n" << std::endl;
     brainTestDog();
-    //    system("leaks a.out");
     return 0;
 }
